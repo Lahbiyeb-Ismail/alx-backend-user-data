295fd308de3ae7037f2d8b51e050225d99a5e048
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-28 14:03:25 +0100

    Formats the log record message by filtering sensitive data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@
 Filter sensitive data from a message based on specified fields
 """
 
+import logging
 import re
 from typing import List
 
@@ -34,3 +35,33 @@ def filter_datum(
                     user_data[i].split("=")[1], redaction, user_data[i]
                 )
     return separator.join(user_data)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record message by filtering sensitive data.
+
+        Args:
+          record (logging.LogRecord): The log record to be formatted.
+
+        Returns:
+          str: The formatted log record message.
+
+        """
+
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+        )
+
+        return super().format(record)
