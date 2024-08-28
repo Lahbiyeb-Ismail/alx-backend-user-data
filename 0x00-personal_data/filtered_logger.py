b6d3c576d861c45c08b6867c7211a7a223bbbb97
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-28 18:27:51 +0100

    3. Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,32 +12,6 @@ from typing import List
 
 import mysql.connector
 
-# def filter_datum(
-#     fields: List[str], redaction: str, message: str, separator: str
-# ) -> str:
-#     """
-#     Filter sensitive data from a message based on specified fields.
-
-#     Args:
-#       fields (List[str]): A list of fields to filter.
-#       redaction (str): The string to replace the filtered data with.
-#       message (str): The message containing the data to be filtered.
-#       separator (str): The separator used to split the message
-#       into data segments.
-
-#     Returns:
-#       str: The filtered message with sensitive data replaced.
-
-#     """
-# user_data = message.split(separator)
-# for i in range(len(user_data)):
-#     for field in fields:
-#         if field in user_data[i]:
-#             user_data[i] = re.sub(
-#                 user_data[i].split("=")[1], redaction, user_data[i]
-#             )
-# return separator.join(user_data)
-
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
@@ -87,10 +61,6 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Retrieves a connection to the personal data database.
-
-    Returns:
-        mysql.connector.connection.MySQLConnection:
-        A connection to the personal data database.
     """
 
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
