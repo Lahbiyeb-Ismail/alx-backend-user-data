945d4b74e783b2ad9b527e72d4fff66f84947042
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-12 13:12:12 +0100

    test file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,9 +43,11 @@ class Auth:
         """
         try:
             fetched_user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode("utf-8"), fetched_user.hashed_password
+            )
         except NoResultFound:
             return False
-        return bcrypt.checkpw(password.encode("utf-8"), fetched_user.hashed_password)
 
     def create_session(self, email: str) -> str:
         """The method should find the user corresponding to the email,
